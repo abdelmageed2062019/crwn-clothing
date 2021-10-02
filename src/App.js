765cabb4b4bdcfd770ca27d092829df389507134
abdelmageed2062019 <68792061+abdelmageed2062019@users.noreply.github.com>
2021-10-02 09:28:14 +0200

    update menu  item with adding withrouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import{ Route } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component.jsx'
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
     </div>
   );
 }
